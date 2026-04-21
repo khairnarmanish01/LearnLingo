955935b1f566723d091ca8781b5d43f03012b8ca
Manish Khairnar <khairnarmanish08@gmail.com>
2026-04-21 20:20:43 +0530

    Initial commit: LearnLingo project setup

---
+++ app/src/main/java/com/example/learnlingo/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.learnlingo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.learnlingo.navigation.AppNavGraph
+import com.example.learnlingo.ui.theme.LearnLingoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            LearnLingoTheme {
+                AppNavGraph()
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/learnlingo/navigation/AppNavGraph.kt
@@ -0,0 +1,12 @@
+package com.example.learnlingo.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun AppNavGraph(navController: NavHostController = rememberNavController()) {
+
+
+
+}
